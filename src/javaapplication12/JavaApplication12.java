9cd4e89019ea089d9fa71d6497aae0c9dc33f491
Aldo Wiranata <awiranata263@gmail.com>
2024-12-24 15:29:19 +0700

    add : javaapplication11

---
+++ src/javaapplication12/JavaApplication12.java
@@ -0,0 +1,13 @@
+package javaapplication12;
+public class JavaApplication12 {
+    public static void main(String[] args) {
+        String name = "Miftahul Jannah";
+        System.out.println("Name = " + name);
+        int umur = 18;
+        System.out.println("Umur = " + umur + " tahun");
+                        
+
+
+    }
+    
+}
---
+++ src/javaapplication13/JavaApplication13.java
@@ -0,0 +1,12 @@
+package javaapplication13;
+public class JavaApplication13 {
+    public static void main(String[] args) {
+     int x = 10;
+     int y = 25;
+     System.out.println(x+y);
+     int z = 20;
+     int saya = 13;
+     System.out.println(z+saya);
+    }
+    
+}
