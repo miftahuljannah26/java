c3877d8f9109e92a82f2f32f3f9b949849b06398
Miftahul Jannah <miftahuljannah6778@gmail.com>
2025-04-25 15:49:07 +0700

    Add files via upload

---
+++ src/Percabangan1MJ.java
@@ -0,0 +1,18 @@
+
+package percabangan1mj;
+
+public class Percabangan1MJ {
+
+    public static void main(String[] args) {
+        int nilai = 90;
+        if (nilai >= 96){
+            System.out.println("Lulus");
+        }else if (nilai <= 70){
+            System.out.println("Mengulang");
+        }else{
+            System.out.println("Tidak Lulus");
+        }
+    
+    }
+    
+}
