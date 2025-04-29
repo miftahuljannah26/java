69f281720cb853b01f475506ab7ce3636daecf82
Miftahul Jannah <miftahuljannah6778@gmail.com>
2025-04-29 15:35:29 +0700

    Add files via upload

---
+++ src/percabangan2.java
@@ -0,0 +1,14 @@
+// menentukan bilangan ganjil genap
+
+package percabangan;
+public class percabangan2 {
+    public static void main (String[]args){
+        int angka = 10;
+        if (angka %2 == 0){
+            System.out.println("Genap");
+        }else{
+            System.out.println("Ganjil");
+        }
+    }
+    
+}
